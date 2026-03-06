894bf6f14a8b662aa7af350463a1446ba9fb8334
anjaligovathoti1405 <anjaligovathoti8@gmail.com>
2026-03-06 14:44:31 +0530

    Add temperature conversion function

---
+++ c3.js
@@ -0,0 +1,16 @@
+function convertTemp() {
+    let temp = document.getElementById("tempInput").value;
+    let unit = document.getElementById("unit").value;
+    let result;
+
+    if (unit === "cToF") {
+        result = (temp * 9/5) + 32;
+        document.getElementById("result").innerHTML =
+            temp + " °C = " + result.toFixed(2) + " °F";
+    } 
+    else {
+        result = (temp - 32) * 5/9;
+        document.getElementById("result").innerHTML =
+            temp + " °F = " + result.toFixed(2) + " °C";
+    }
+}
